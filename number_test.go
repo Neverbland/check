f1674bd0df688bf3572ecbf876f87fbf71db662e
Tsarikau Eugeny <tsarikau@gmail>
2015-03-13 12:20:19 +0100

    fix typo

--- number_test.go
+++ number_test.go
@@ -7,7 +7,7 @@ func TestNumberValidators(t *testing.T) {
 		{Number{LowerThan{2}}, 1, true},
 		{Number{LowerThan{1}}, 2, false},
 		{Number{LowerThan{2}}, 2, false},
-		{Number{GreaterThan{1}}, 0, true},
+		{Number{GreaterThan{1}}, 2, true},
 		{Number{GreaterThan{2}}, 1, false},
 		{Number{GreaterThan{2}}, 2, false},
 	}
