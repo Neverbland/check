0e79027f159510c267886d5919e8a4ca883706e7
Tsarikau Eugeny <tsarikau@gmail>
2015-04-17 14:25:43 +0300

    enum,equal

---
+++ value.go
@@ -0,0 +1,34 @@
+package check
+
+import (
+	"reflect"
+)
+
+type Enum []interface{}
+
+func (validator Enum) Validate(v interface{}) Error {
+	if len(validator) == 0 {
+		return nil
+	}
+
+	for _, val := range validator {
+		if err := (Equal{val}).Validate(v); err == nil {
+			return nil
+		}
+	}
+
+	return ValidationErr("enum", "must be equal to one of %v. Given %v", validator, v)
+}
+
+type Equal struct {
+	Value interface{}
+}
+
+func (validator Equal) Validate(v interface{}) Error {
+
+	if !reflect.DeepEqual(v, validator.Value) {
+		return ValidationErr("equal", "must be equal to %v. Given %v", validator.Value, v)
+	}
+
+	return nil
+}
