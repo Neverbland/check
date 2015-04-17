02c7ac0b90884e2c328547b9d760ba113aa78e4d
Tsarikau Eugeny <tsarikau@gmail>
2015-04-17 14:25:30 +0300

    few core middlewares

--- check.go
+++ check.go
@@ -59,3 +59,37 @@ type Callback func(interface{}) Error
 func (validator Callback) Validate(v interface{}) Error {
 	return validator(v)
 }
+
+type Normalizer func(interface{}) (interface{}, error)
+
+//Pass value through Normalizer callbacks chain and pass it to inner validator
+type Normalize struct {
+	Normalizers []Normalizer
+	Validator   Validator
+}
+
+func (validator Normalize) Validate(v interface{}) Error {
+
+	for _, fn := range validator.Normalizers {
+		if val, err := fn(v); err != nil {
+			return ValidationErr("normalize", err.Error())
+		} else {
+			v = val
+		}
+	}
+
+	return validator.Validator.Validate(v)
+}
+
+//Overrides error if present
+type CustomError struct {
+	Validator
+	Error Error
+}
+
+func (validator CustomError) Validate(v interface{}) Error {
+	if err := validator.Validator.Validate(v); err != nil {
+		return validator.Error
+	}
+	return nil
+}
